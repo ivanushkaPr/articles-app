963ef0037793f4025c153ae5eae6e596992c9f86
ivan krasheninnikov <exitusletalis2008@yandex.ru>
2023-05-25 15:47:40 +0300

    [implement] - реализовал эмуляцию загрузки статей и preloader.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,74 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-if="isArticlesDownloaded">
+      {{this.$store.state.articles}}
+    </div>
+    <Preloader v-else/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import { mapState } from 'vuex';
+import Preloader from './components/Preloader.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    Preloader,
+  },
+  computed: {
+    ...mapState({
+      isArticlesDownloaded: (state) => state.articles.length > 0,
+    }),
+  },
+  async mounted() {
+    debugger;
+    const response = await fetch('articles.json');
+    if (response.ok) {
+      const json = await response.json();
+      setTimeout(() => {
+        this.$store.commit('setArticles', json);
+      }, 2000);
+    } else {
+      alert(`Ошибка HTTP: ${response.status}`);
+    }
   },
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  margin: 0px;
 }
+
+#app {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.preloader {
+  width: 36px;
+  height: 36px;
+  border: 4px solid #ED5252;
+  border-radius: 50%;
+  border-top-color: transparent;
+  transform: rotate(-45deg);
+  animation-duration: 100s;
+  animation-name: infinite-rotation;
+  animation-iteration-count: infinite;
+}
+
+@keyframes infinite-rotation {
+  from {
+    transform: rotate(-10000deg);
+  }
+
+  to {
+    transform: rotate(0deg);
+  }
+}
+
 </style>
---
+++ src/components/Preloader.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="layout">
+    <span class="layout__preloader"></span>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+.layout {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.layout__preloader {
+  width: 36px;
+  height: 36px;
+  border: 4px solid #ED5252;
+  border-radius: 50%;
+  border-top-color: transparent;
+  animation-duration: 100s;
+  animation-name: infinite-rotation;
+  animation-iteration-count: infinite;
+  animation-timing-function: linear;
+}
+
+@keyframes infinite-rotation {
+  from {
+    transform: rotate(-45deg);
+  }
+
+  to {
+    transform: rotate(10000deg);
+  }
+}
+
+</style>
